f76cc4f065af5237b08591e5e318f8f6fbecf2c4
Sudarshan Gharti <154953805+lynx-fx@users.noreply.github.com>
2024-10-15 14:56:35 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './Components/Weather.jsx'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
